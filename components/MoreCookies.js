e35938a3c47abcb343095718e2cd88215f5c782a
Ian McPhail <ian@clerk.dev>
2022-01-31 16:52:08 -0500

    Implement custom hooks

--- components/MoreCookies.js
+++ components/MoreCookies.js
@@ -1,12 +1,15 @@
 import dynamic from "next/dynamic";
+import { useScoreboard } from "../hooks";
 const CookieClicker = dynamic(() => import("react-cookie-clicker"), {
   ssr: false
 });
 
 const MoreCookies = () => {
+  const [count, increment] = useScoreboard();
+
   return (
     <div style={{ marginTop: 150 }}>
-      <CookieClicker />
+      <CookieClicker count={count} onClick={increment} />
       <h2>Click the cookie</h2>
     </div>
   );
--- hooks/index.js
+++ hooks/index.js
@@ -1,4 +1,5 @@
 import { request } from "graphql-request";
+import { useEffect, useState } from "react";
 import { useSession } from "@clerk/nextjs";
 import useSWR from "swr";
 
@@ -17,8 +18,59 @@ import useSWR from "swr";
   const endpoint = process.env.NEXT_PUBLIC_HASURA_GRAPHQL_API;
   const fetcher = async () =>
     request(endpoint, query, variables, {
-      authorization: `Bearer ${await getToken({ template: "your-template-name" })}`
+      authorization: `Bearer ${await getToken({ template: "hasura" })}`
     });
 
   return useSWR(query, blockRequest ? () => {} : fetcher);
+};
+
+/**
+ * Make authenticated GraphQL upsert mutation with count
+ * @param {number} count New count to set
+ * @param {Object} data  Previous scoreboard count data 
+ * @returns 
+ */
+export const useCountMutation = (count, data) => {
+  const prevCount = data?.scoreboard[0]?.count ?? 0;
+  const blockRequest = count < 1 || prevCount === count;
+
+  // Block mutation if count is less than 1 or equal to previous value
+  return useQuery(
+    `mutation {
+      insert_scoreboard_one(
+        object: { count: ${count} }, 
+        on_conflict: { constraint: scoreboard_pkey, update_columns: count }) {
+        count
+        user_id
+      }
+  }`,
+    null,
+    blockRequest
+  );
+};
+
+/**
+ * Keep track of current count and provide increment function
+ * @returns {[ count: number, increment: () => void]}
+ */
+export const useScoreboard = () => {
+  const [count, setCount] = useState(0);
+  const { data } = useQuery(`query {
+    scoreboard { count, user_id }
+  }`);
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  // Perform mutation on count
+  useCountMutation(count, data);
+
+  useEffect(() => {
+    if (!count && data?.scoreboard[0]) {
+      // Set initial count from database
+      setCount(data.scoreboard[0].count);
+    }
+  }, [count, data]);
+
+  return [count, increment];
 };
